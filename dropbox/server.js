913dfe2c2603cf3d3f93fd250950d65768d6ebc3
AnnaZHoner <35963798+AnnaZHoner@users.noreply.github.com>
2019-04-08 13:13:15 +0100

    add async functions
    Functions running in async - cannot add folder memeber

--- dropbox/server.js
+++ dropbox/server.js
@@ -11,83 +11,79 @@ var CryptoJS = require("crypto-js");
 var AES = require("crypto-js/aes");
 var SHA256 = require("crypto-js/sha256");
 var Dropbox = require('dropbox').Dropbox;
-// var ACCESS_TOKEN = localStorage.accessToken;   //in case I want to load in the access token remotely later
 var dbx = new Dropbox({ accessToken: 'DyQ1AM63lPAAAAAAAAAAjcKhGpnTxEfJkjaTh6skBrOszanCrbtVhfjdkgJHxZVK' });
+var newFolder = 'group folder03'
+var sharedId = ''
+var sharingAddFolderMember = {};
 
 
-dbx.filesListFolder({ path: '' })
-  .then(function (response) {
-    //console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-
-dbx.filesListFolder({ path: '' })
-  .then(function (response) {
-    // console.log(response.entries);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
-
-
-//-----------------------------Taking in file------------------------------------//
+//Taking in file
 var fs = require('fs');
 var textFile = fs.readFileSync('file.txt')
 var text = textFile.toString()
-//txt file taken in okay doke
 
-
-//------------------------CIPHERING-------------------------------------------------//
+//CIPHERING
 var ciphertext = CryptoJS.AES.encrypt(text, 'secret key 123');
-console.log("This is the message pre-encryption: " + text);
-console.log("This is the message encrypted: " + ciphertext);
 var bytes = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
-//console.log(ciphertext)
 var plaintext = bytes.toString(CryptoJS.enc.Utf8);
-console.log("This is the message decrypted: " + plaintext);
 
+//Running everthing in asyn 
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
 
-//-----------------------------FILE UPLOADING-------------------------------------//
-
-
-//attempting to create folder first 
-dbx.filesCreateFolder({ path: '/sharedfolderbutnotshared2' })
-  .then(function (response) {
-    dbx.sharingShareFolder({
-      path: response['path_lower'],
-      shared_link_policy: 'anyone',
-      actions: ['invite_editor'],
-    })
-     
-
-    });
-
+//commit file
 var fileCommitInfo = {};
 fileCommitInfo.contents = ciphertext;
-fileCommitInfo.path = '/sharedfolderbutnotshared2/test text.txt';
+fileCommitInfo.path = '/' + newFolder + '/text.txt';
 fileCommitInfo.mode = { '.tag': 'overwrite' };
 fileCommitInfo.autorename = true;
 fileCommitInfo.mute = true;
+fileCommitInfo.strict_conflict = true;
 
 
-var sharingAddFolderMember = {};
-sharingAddFolderMember.shared_folder_id = 'new folder'
-sharingAddFolderMember.members = ['sabahk32@hotmail.com'].email
-sharingAddFolderMember.quiet = false
+async function createFolder() {
+  try {
+    var response = await dbx.sharingShareFolder({ path: '/' + newFolder })
+    console.log('folder created')
+  }
+  catch (e) { console.log('folder was not created' + JSON.stringify(e)) }
+  var result = await resolveAfter2Seconds();
+}
 
 
-dbx.filesUpload(fileCommitInfo)
-  .then(function (response) {
-    //console.log(response);
-  })
-  .catch(function (errr) {
-    console.log(errr);
-  });
+async function upload() {
+  try {
+    var response = await dbx.filesUpload(fileCommitInfo)
+    sharedId = response.parent_shared_folder_id
+  }
+  catch (e) { console.log('your file upload failed' + JSON.stringify(e)) }
+}
 
-//------------------------------Group Info------------------------------------------//
 
-//
+async function sharing() {
+  console.log('shared folder id: ' + sharedId)
+  sharingAddFolderMember.shared_folder_id = sharedId
+  sharingAddFolderMember.members = [{ member: { email: 'honera@tcd.ie', '.tag': 'email' }, access_level: { '.tag': 'editor' } }]
+  sharingAddFolderMember.quiet = false
+  try {
+    var responseSharing = await dbx.sharingShareFolder(sharingAddFolderMember)
+    console.log(responseSharing)
+  }
+  catch (e) { console.log('folder failed to share' + JSON.stringify(e)) }
+}
 
+
+async function start() {
+  console.log('calling');
+  await createFolder();
+  await upload();
+  await sharing();
+  // console.log(result);
+}
+
+start();
