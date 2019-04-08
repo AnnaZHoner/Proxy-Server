26d3e2822cdd2aa636e71a7a503fa8ae95772675
AnnaZHoner <35963798+AnnaZHoner@users.noreply.github.com>
2019-04-08 19:07:49 +0100

    Sharing added
    Can now add accounts to folder

--- dropbox/server.js
+++ dropbox/server.js
@@ -1,7 +1,11 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 var fs = require('fs');
 require('es6-promise').polyfill();
 require('isomorphic-fetch'); // or another library of choice.
-const readline = require('readline')
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,9 +16,12 @@ var AES = require("crypto-js/aes");
 var SHA256 = require("crypto-js/sha256");
 var Dropbox = require('dropbox').Dropbox;
 var dbx = new Dropbox({ accessToken: 'DyQ1AM63lPAAAAAAAAAAjcKhGpnTxEfJkjaTh6skBrOszanCrbtVhfjdkgJHxZVK' });
-var newFolder = 'group folder03'
+var newFolder = 'group folder0'
 var sharedId = ''
 var sharingAddFolderMember = {};
+var sharingRemoveFolderMember = {};
+var enteredEmail = 'honera@tcd.ie'
+
 
 
 //Taking in file
@@ -27,12 +34,12 @@ var ciphertext = CryptoJS.AES.encrypt(text, 'secret key 123');
 var bytes = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
 var plaintext = bytes.toString(CryptoJS.enc.Utf8);
 
-//Running everthing in asyn 
+//add function delay
 function resolveAfter2Seconds() {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve('resolved');
-    }, 2000);
+    }, 1000);
   });
 }
 
@@ -40,12 +47,13 @@ function resolveAfter2Seconds() {
 var fileCommitInfo = {};
 fileCommitInfo.contents = ciphertext;
 fileCommitInfo.path = '/' + newFolder + '/text.txt';
+//fileCommitInfo.mode = {update:'conflicted copy', '.tag': 'update'}
 fileCommitInfo.mode = { '.tag': 'overwrite' };
 fileCommitInfo.autorename = true;
 fileCommitInfo.mute = true;
 fileCommitInfo.strict_conflict = true;
 
-
+//run create folder
 async function createFolder() {
   try {
     var response = await dbx.sharingShareFolder({ path: '/' + newFolder })
@@ -55,35 +63,68 @@ async function createFolder() {
   var result = await resolveAfter2Seconds();
 }
 
-
+//run upload
 async function upload() {
   try {
     var response = await dbx.filesUpload(fileCommitInfo)
+    console.log(response)
     sharedId = response.parent_shared_folder_id
   }
   catch (e) { console.log('your file upload failed' + JSON.stringify(e)) }
 }
 
-
-async function sharing() {
+//run sharing
+async function sharing(enteredEmail) {
   console.log('shared folder id: ' + sharedId)
   sharingAddFolderMember.shared_folder_id = sharedId
-  sharingAddFolderMember.members = [{ member: { email: 'honera@tcd.ie', '.tag': 'email' }, access_level: { '.tag': 'editor' } }]
-  sharingAddFolderMember.quiet = false
+  sharingAddFolderMember.members = [{ member: { email: enteredEmail, '.tag': 'email' }, access_level: { '.tag': 'editor' } }]
+  sharingAddFolderMember.quiet = true
   try {
-    var responseSharing = await dbx.sharingShareFolder(sharingAddFolderMember)
-    console.log(responseSharing)
+    var responseSharing = await dbx.sharingAddFolderMember(sharingAddFolderMember)
+    //console.log(responseSharing)
   }
   catch (e) { console.log('folder failed to share' + JSON.stringify(e)) }
 }
+async function addAnother() {
+  readline.question(`Would you like to enter another email?(Y\N) `, (ans) => {
+    if (ans == 'Yes' ||ans == 'Y' ||ans == 'yes' ||ans == 'y')
+    {
+      readline.question(`Enter the email now: `, (enteredEmail) => {
+        console.log('\n' + enteredEmail + ' will now be joined to the folder')
+        sharing(enteredEmail);
+        addAnother();
+        //readline.close()
+      })
+    }
+    else {
+      console.log('Goodbye')
+      
+    }
+  })
+}
 
+async function removing() {
+  console.log('removing MEMEbers ')
+  sharingRemoveFolderMember.shared_folder_id = sharedId
+  sharingRemoveFolderMember.member = [{email: 'honera@tcd.ie'}]
+  sharingRemoveFolderMember.leave_a_copy = false
+  try {
+    var responseRemoving = await dbx.sharingRemoveFolderMember(sharingRemoveFolderMember)
+    //console.log(responseSharing)
+  }
+  catch (e) { console.log('failed to remove member' + JSON.stringify(e)) }
+
+}
 
 async function start() {
   console.log('calling');
   await createFolder();
   await upload();
-  await sharing();
-  // console.log(result);
+  await sharing(enteredEmail);
+  addAnother();
+  //await removing();
 }
 
+
 start();
+//readline.close()
